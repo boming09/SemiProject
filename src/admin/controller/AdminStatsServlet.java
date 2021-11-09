336a78ed6cd38bbee729249657759f78b7f7c173
윤용한 <yoonyh95@naver.com>
2021-11-09 22:05:08 +0900

    asdasd

--- src/admin/controller/AdminStatsServlet.java
+++ src/admin/controller/AdminStatsServlet.java
@@ -2,6 +2,7 @@ package admin.controller;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class AdminGenderServlet
+ * Servlet implementation class AdminStatsServlet
  */
 @WebServlet("/admin/stats")
 public class AdminStatsServlet extends HttpServlet {
@@ -27,7 +28,8 @@ public class AdminStatsServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/views/adminpage/adminStats.jsp").forward(request, response);
+		RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/views/adminpage/adminStats.jsp");
+		view.forward(request, response);
 	}
 
 	/**
